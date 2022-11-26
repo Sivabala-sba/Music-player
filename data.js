2b91349b4c8a34d5d4749b5301c874272f986528
Sivabala-SBA <87258592+Sivabala-sba@users.noreply.github.com>
2022-11-26 21:32:49 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'CEO in the house',
+        path: 'musics/CEO.mp3',
+        artist: 'artist 1',
+        cover: 'img/CEO.jpg'
+    },
+    {
+        name: 'Aaluma Doluma',
+        path: 'musics/AalumaDoluma.mp3',
+        artist: 'artist 2',
+        cover: 'img/aalumadoluma.jpg'
+    },
+    {
+        name: 'Kadhal Cricket',
+        path: 'musics/KadhalCricket.mp3',
+        artist: 'artist 3',
+        cover: 'img/kadhalcricket.jpg'
+    },
+    {
+        name: 'Osaka Osaka',
+        path: 'musics/OsakaOsaka.mp3',
+        artist: 'artist 4',
+        cover: 'img/osakaosaka.jpg'
+    },
+    {
+        name: 'The Karma Theme',
+        path: 'musics/TheKarmaTheme.mp3',
+        artist: 'artist 5',
+        cover: 'img/thekarmatheme.jpg'
+    }
+]
